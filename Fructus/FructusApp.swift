68a278396245797af9521abc68ca203955da3d5a
Zachary <zackj957@gmail.com>
2020-10-06 19:06:38 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Zachary Jensen on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
